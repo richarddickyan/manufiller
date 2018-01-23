e0b1c0bcb43f23264b2ed902cc8d72bc7cd97c61
richarddickyan <35725340+richarddickyan@users.noreply.github.com>
2018-01-23 02:01:29 -0800

    Add files via upload
    Added template, 1_calendar and 2_email versions

---
+++ template/content.js
@@ -0,0 +1,10 @@
+console.log("Chrome extension go");
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+  console.log(message.txt);
+  if (message.txt === "hello") {
+
+  }
+}
